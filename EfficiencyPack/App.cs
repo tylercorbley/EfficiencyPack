d7e38e12441d95c0e8e9924a454c41509058541a
tylercorbley <tylercorbley@gmail.com>
2023-06-04 12:18:37 -0400

    created ribbon and panel

--- EfficiencyPack/App.cs
+++ EfficiencyPack/App.cs
@@ -1,13 +1,7 @@
 #region Namespaces
-using Autodesk.Revit.ApplicationServices;
-using Autodesk.Revit.Attributes;
-using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Runtime.Versioning;
-using System.Windows.Markup;
 
 #endregion
 
@@ -17,26 +11,24 @@ namespace EfficiencyPack
     {
         public Result OnStartup(UIControlledApplication app)
         {
-            //// 1. Create ribbon tab
-            //try
-            //{
-            //    app.CreateRibbonTab("My First Revit Add-in");
-            //}
-            //catch (Exception)
-            //{
-            //    Debug.Print("Tab already exists.");
-            //}
+            // 1. Create ribbon tab
+            try
+            {
+                app.CreateRibbonTab("Efficiency Pack");
+            }
+            catch (Exception)
+            {
+                Debug.Print("Tab already exists.");
+            }
 
-            //// 2. Create ribbon panel 
-            //RibbonPanel panel = Utils.CreateRibbonPanel(app, "My First Revit Add-in", "Revit Tools");
+            // 2. Create ribbon panel 
+            RibbonPanel panel000 = Utils.CreateRibbonPanel(app, "Efficiency Pack", "000 - Misc");
 
-            //// 3. Create button data instances
-            //ButtonDataClass myButtonData1 = new ButtonDataClass("btnArchSmarter_Addin_Template_2023_1", "Button 1", Command1.GetMethod(), Properties.Resources.Blue_32, Properties.Resources.Blue_16, "Tooltip 1");
-            //ButtonDataClass myButtonData2 = new ButtonDataClass("btnArchSmarter_Addin_Template_2023_2", "Button 2", Command2.GetMethod(), Properties.Resources.Red_32, Properties.Resources.Red_16, "Tooltip 2");
+            // 3. Create button data instances
+            ButtonDataClass myButtonData1 = new ButtonDataClass("btnArchSmarter_Addin_Template_2023_1", "Forest Generator", ForestGen.GetMethod(), Properties.Resources.Blue_32, Properties.Resources.Blue_16, "Draw detail lines into a polygonal shape for your trees to be placed inside. Follow the command prompts to define amount and type.");
 
-            //// 4. Create buttons
-            //PushButton myButton1 = panel.AddItem(myButtonData1.Data) as PushButton;
-            //PushButton myButton2 = panel.AddItem(myButtonData2.Data) as PushButton;
+            // 4. Create buttons
+            PushButton myButton1 = panel000.AddItem(myButtonData1.Data) as PushButton;
 
             // NOTE:
             // To create a new tool, copy lines 35 and 39 and rename the variables to "myButtonData3" and "myButton3". 
