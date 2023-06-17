46454f9df372248da7d507d94649a1e1a198f0e7
tylercorbley <tylercorbley@gmail.com>
2023-06-17 12:24:56 -0400

    touched up some things. still in prog but very close.

--- EfficiencyPack/DuplicateSheet.cs
+++ EfficiencyPack/DuplicateSheet.cs
@@ -50,7 +50,7 @@ namespace EfficiencyPack
 
                 // Create a new sheet
                 ViewSheet newSheet = ViewSheet.Create(doc, familyInstance.Symbol.Id);
-                newSheet.Name = activeSheet.Name + "_COPY";
+                newSheet.Name = GetUniqueSheetName(doc, activeSheet.Name);
 
                 // Duplicate each view on the active sheet
                 foreach (ElementId viewId in activeSheet.GetAllPlacedViews())
@@ -59,13 +59,21 @@ namespace EfficiencyPack
 
                     if (view is ViewPlan || view is ViewSection)
                     {
+                        // Duplicate the view
                         ElementId copiedViewId = ElementTransformUtils.CopyElement(doc, viewId, new XYZ(0, 0, 0)).FirstOrDefault();
                         if (copiedViewId != null)
                         {
                             Element copiedView = doc.GetElement(copiedViewId);
-                            copiedView.Name = view.Name + "_COPY";
+                            copiedView.Name = GetUniqueViewName(doc, view.Name);
 
-                            // Place the copied view on the new sheet
+                            // Apply the view template from the original view to the duplicated view
+                            ElementId viewTemplateId = GetViewTemplateId(doc, view);
+                            if (viewTemplateId != ElementId.InvalidElementId)
+                            {
+                                ApplyViewTemplate(doc, copiedViewId, viewTemplateId);
+                            }
+
+                            // Place the duplicated view on the new sheet
                             XYZ viewportPosition = GetViewportPosition(doc, activeSheet, viewId);
                             if (viewportPosition != null)
                             {
@@ -103,6 +111,32 @@ namespace EfficiencyPack
             return null;
         }
 
+        // Helper method to retrieve the view template ID of a view
+        private ElementId GetViewTemplateId(Document doc, Element view)
+        {
+            Parameter parameter = view.get_Parameter(BuiltInParameter.VIEW_TEMPLATE_FOR_SCHEDULE);
+            if (parameter != null && parameter.HasValue)
+            {
+                return parameter.AsElementId();
+            }
+            return ElementId.InvalidElementId;
+        }
+
+        // Helper method to apply the view template to a view
+        private void ApplyViewTemplate(Document doc, ElementId viewId, ElementId viewTemplateId)
+        {
+            View view = doc.GetElement(viewId) as View;
+            if (view != null)
+            {
+                using (Transaction trans = new Transaction(doc))
+                {
+                    //trans.Start("Apply View Template");
+                    view.ViewTemplateId = viewTemplateId;
+                    //trans.Commit();
+                }
+            }
+        }
+
         // Custom selection filter to filter out non-title block elements during selection
         private class TitleBlockSelectionFilter : ISelectionFilter
         {
@@ -119,6 +153,52 @@ namespace EfficiencyPack
             {
                 return false;
             }
+        }
+
+        // Helper method to generate a unique sheet name by appending a number if necessary
+        private string GetUniqueSheetName(Document doc, string baseName)
+        {
+            string uniqueName = baseName;
+            int counter = 1;
+
+            while (GetSheetByName(doc, uniqueName) != null)
+            {
+                uniqueName = $"{baseName}_{counter}";
+                counter++;
+            }
+
+            return uniqueName;
+        }
+
+        // Helper method to retrieve a sheet by name
+        private ViewSheet GetSheetByName(Document doc, string name)
+        {
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            collector.OfClass(typeof(ViewSheet));
+            return collector.Cast<ViewSheet>().FirstOrDefault(sheet => sheet.Name.Equals(name));
+        }
+
+        // Helper method to generate a unique view name by appending a number if necessary
+        private string GetUniqueViewName(Document doc, string baseName)
+        {
+            string uniqueName = baseName;
+            int counter = 1;
+
+            while (GetViewByName(doc, uniqueName) != null)
+            {
+                uniqueName = $"{baseName}_{counter}";
+                counter++;
+            }
+
+            return uniqueName;
+        }
+
+        // Helper method to retrieve a view by name
+        private Element GetViewByName(Document doc, string name)
+        {
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            collector.OfClass(typeof(View));
+            return collector.Cast<View>().FirstOrDefault(view => view.Name.Equals(name));
         }
         public static String GetMethod()
         {
