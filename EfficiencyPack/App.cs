a67221b2a08d11ed121c281c157daab4f9d1d8e5
tylercorbley <tylercorbley@gmail.com>
2023-06-20 20:46:48 -0400

    added FloorByDepartment button, working.

--- EfficiencyPack/App.cs
+++ EfficiencyPack/App.cs
@@ -29,10 +29,12 @@ namespace EfficiencyPack
             ButtonDataClass myButtonData1 = new ButtonDataClass("ForestGen", "Forest Generator", ForestGen.GetMethod(), Properties.Resources.Blue_32, Properties.Resources.Blue_16, "Draw detail lines into a polygonal shape for your trees to be placed inside. Follow the command prompts to define amount and type.");
             ButtonDataClass myButtonData2 = new ButtonDataClass("FloorByRoom", "Floors by Rooms", FloorByRoom.GetMethod(), Properties.Resources.Blue_32, Properties.Resources.Blue_16, "Select rooms. Run command and follow prompt to select type of floor to populate.");
             ButtonDataClass myButtonData3 = new ButtonDataClass("DuplicateSheet", "Duplicate Sheet", DuplicateViewsCommand.GetMethod(), Properties.Resources.Blue_32, Properties.Resources.Blue_16, "Select Sheet. Run command.");
+            ButtonDataClass myButtonData4 = new ButtonDataClass("FloorByDepartment", "Floors by Department", FloorByDepartment.GetMethod(), Properties.Resources.Blue_32, Properties.Resources.Blue_16, "Select rooms. Run command. Get Floors.");
 
             // 4. Create buttons
             PushButton myButton1 = panel000.AddItem(myButtonData1.Data) as PushButton;
             PushButton myButton3 = panel000.AddItem(myButtonData3.Data) as PushButton;
+            PushButton myButton4 = panel000.AddItem(myButtonData4.Data) as PushButton;
             PushButton myButton2 = panel900.AddItem(myButtonData2.Data) as PushButton;
 
             // NOTE:
---
+++ EfficiencyPack/FloorByDepartment.cs
@@ -0,0 +1,157 @@
+#region Namespaces
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+#endregion
+
+namespace EfficiencyPack
+{
+    [Transaction(TransactionMode.Manual)]
+    public class FloorByDepartment : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            // Get the current Revit application and document
+            UIApplication uiApp = commandData.Application;
+            UIDocument uiDoc = uiApp.ActiveUIDocument;
+            Document doc = uiDoc.Document;
+
+            // Collect all the floor type names
+            List<string> floorTypeNames = CollectFloorTypeNames(doc);
+
+            // Get the selection of rooms in the Revit document
+            ICollection<ElementId> selectedIds = uiDoc.Selection.GetElementIds();
+
+            // Filter the selected elements to get only rooms
+            List<Room> rooms = new List<Room>();
+            foreach (ElementId id in selectedIds)
+            {
+                Element element = doc.GetElement(id);
+                if (element is Room room)
+                {
+                    rooms.Add(room);
+                }
+            }
+
+            // Get all floor types
+            FilteredElementCollector floorTypeCollector = new FilteredElementCollector(doc);
+            ICollection<Element> floorTypes = floorTypeCollector
+                .OfClass(typeof(FloorType))
+                .WhereElementIsElementType()
+                .ToList();
+            int counter = 0;
+            double raiseHeight = .25 / 12;
+            // Iterate through each room and create a floor
+            using (Transaction trans = new Transaction(doc, "Place Floors"))
+            {
+                trans.Start();
+
+                foreach (Room room in rooms)
+                {
+                    // Get the department parameter value of the room
+                    string department = room.get_Parameter(BuiltInParameter.ROOM_DEPARTMENT).AsString();
+
+                    // Find the floor type with matching keynote parameter
+                    FloorType floorType = floorTypes
+                        .Cast<FloorType>()
+                        .FirstOrDefault(ft => ft.get_Parameter(BuiltInParameter.KEYNOTE_PARAM).AsString() == department);
+
+                    if (floorType != null)
+                    {
+                        if (floorType.get_Parameter(BuiltInParameter.KEYNOTE_PARAM).AsString() != null)
+                        {
+
+                            //// Get the room boundary curve
+                            //SpatialElementBoundaryOptions boundaryOptions = new SpatialElementBoundaryOptions();
+                            //IList<IList<BoundarySegment>> boundarySegments = ((Room)room).GetBoundarySegments(boundaryOptions);
+                            //CurveArray curveArray = new CurveArray();
+                            //foreach (IList<BoundarySegment> segments in boundarySegments)
+                            //{
+                            //    foreach (BoundarySegment segment in segments)
+                            //    {
+                            //        curveArray.Append(segment.GetCurve());
+                            //    }
+                            //}
+                            IList<IList<BoundarySegment>> roomBoundaries = room.GetBoundarySegments(new SpatialElementBoundaryOptions());
+                            if (roomBoundaries.Count > 0)
+                            {
+                                foreach (IList<BoundarySegment> boundary in roomBoundaries)
+                                {
+                                    CurveArray curveArray = new CurveArray();
+                                    foreach (BoundarySegment seg in boundary)
+                                    {
+                                        Curve curve = seg.GetCurve();
+                                        curveArray.Append(curve);
+                                    }
+                                    // Get the level of the room
+                                    Level level = doc.GetElement(room.LevelId) as Level;
+
+                                    // Create a new floor using the NewFloor method
+                                    Floor floor = doc.Create.NewFloor(curveArray, floorType, level, false);
+                                    RaiseFloor(floor, raiseHeight);
+                                    counter++;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                trans.Commit();
+            }
+            TaskDialog.Show("OK!", $"You Created {counter} Floors!");
+
+            return Result.Succeeded;
+        }
+
+        private void RaiseFloor(Floor floor, double height)
+        {
+            Parameter heightOffsetParam = floor.get_Parameter(BuiltInParameter.FLOOR_HEIGHTABOVELEVEL_PARAM);
+
+            if (heightOffsetParam != null && heightOffsetParam.StorageType == StorageType.Double)
+            {
+                double currentHeightOffset = heightOffsetParam.AsDouble();
+                double newHeightOffset = currentHeightOffset + height;
+
+                heightOffsetParam.Set(newHeightOffset);
+            }
+        }
+        private List<string> CollectFloorTypeNames(Document doc)
+        {
+            // Collect all the floor types in the document
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            collector.OfClass(typeof(FloorType));
+            List<string> floorTypeNames = collector.Cast<FloorType>().Select(ft => ft.Name).ToList();
+
+            return floorTypeNames;
+        }
+
+        private ElementId GetFloorTypeIdByName(Document doc, string floorTypeName)
+        {
+            // Collect all the floor types in the document
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            collector.OfClass(typeof(FloorType));
+
+            foreach (FloorType floorType in collector)
+            {
+                if (floorType.Name.Equals(floorTypeName))
+                {
+                    return floorType.Id;
+                }
+            }
+
+            return null; // Floor type with the given name not found
+        }
+
+        public static String GetMethod()
+        {
+            var method = MethodBase.GetCurrentMethod().DeclaringType?.FullName;
+            return method;
+        }
+    }
+}
