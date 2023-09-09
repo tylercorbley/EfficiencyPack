c0f0f5fd81eb2223f7d1b7fce834d7e22fc128c4
tylercorbley <tylercorbley@gmail.com>
2023-09-08 20:23:12 -0400

    Added working view generator

--- EfficiencyPack_Shared/App.cs
+++ EfficiencyPack_Shared/App.cs
@@ -28,59 +28,59 @@ namespace EfficiencyPack
             RibbonPanel panel900 = Utils.CreateRibbonPanel(app, "Efficiency Pack", "900 - Finishes");
 
             // 3. Create button data instances
-            ButtonDataClass myButtonData1 = new ButtonDataClass("ForestGen", "Forest \rGenerator", ForestGen.GetMethod(), EfficiencyPack_Resources.Properties.Resources.Blue_32, EfficiencyPack_Resources.Properties.Resources.Blue_16, "Draw detail lines into a polygonal shape for your trees to be placed inside. Follow the command prompts to define amount and type.");
-            ButtonDataClass myButtonData2 = new ButtonDataClass("FloorByRoom", "Floors by \rRooms", FloorByRoom.GetMethod(), EfficiencyPack_Resources.Properties.Resources.Blue_32, EfficiencyPack_Resources.Properties.Resources.Blue_16, "Select rooms. Run command and follow prompt to select type of floor to populate.");
+            ButtonDataClass BtnForestGen = new ButtonDataClass("ForestGen", "Forest \rGenerator", ForestGen.GetMethod(), EfficiencyPack_Resources.Properties.Resources.Blue_32, EfficiencyPack_Resources.Properties.Resources.Blue_16, "Draw detail lines into a polygonal shape for your trees to be placed inside. Follow the command prompts to define amount and type.");
+            ButtonDataClass BtnFloorByRoom = new ButtonDataClass("FloorByRoom", "Floors by \rRooms", FloorByRoom.GetMethod(), EfficiencyPack_Resources.Properties.Resources.Blue_32, EfficiencyPack_Resources.Properties.Resources.Blue_16, "Select rooms. Run command and follow prompt to select type of floor to populate.");
 #if REVIT2022
-            ButtonDataClass myButtonData3 = new ButtonDataClass("DuplicateSheet", "Duplicate \rSheet", DuplicateViewsCommand.GetMethod(), EfficiencyPack_Resources.Properties.Resources.Blue_32, EfficiencyPack_Resources.Properties.Resources.Blue_16, "Select Sheet. Run command.");
+            ButtonDataClass BtnDuplicateSheet = new ButtonDataClass("DuplicateSheet", "Duplicate \rSheet", DuplicateViewsCommand.GetMethod(), EfficiencyPack_Resources.Properties.Resources.Blue_32, EfficiencyPack_Resources.Properties.Resources.Blue_16, "Select Sheet. Run command.");
 #endif
-            ButtonDataClass myButtonData4 = new ButtonDataClass("FloorByDepartment", "Floors by \rDepartment", FloorByDepartment.GetMethod(), EfficiencyPack_Resources.Properties.Resources.Blue_32, EfficiencyPack_Resources.Properties.Resources.Blue_16, "Select rooms. Run command. Get Floors.");
-            ButtonDataClass myButtonData5 = new ButtonDataClass("LinesByRoom", "Lines by \rRoom", LinesByRoom.GetMethod(), EfficiencyPack_Resources.Properties.Resources.Blue_32, EfficiencyPack_Resources.Properties.Resources.Blue_16, "Select rooms. Run command. Get Outline.");
-            ButtonDataClass myButtonData6 = new ButtonDataClass("RoomPlanGen", "Plans by \rRoom", RoomPlanGen.GetMethod(), EfficiencyPack_Resources.Properties.Resources.Blue_32, EfficiencyPack_Resources.Properties.Resources.Blue_16, "Select rooms. Run command. Get Plans.");
-            ButtonDataClass myButtonData7 = new ButtonDataClass("CenterRoom", "Center\rRoom", RoomTagCenteringCommand.GetMethod(), EfficiencyPack_Resources.Properties.Resources.Blue_32, EfficiencyPack_Resources.Properties.Resources.Blue_16, "Select rooms. Center rooms.");
-            ButtonDataClass myButtonData8 = new ButtonDataClass("CenterRoomTag", "Center\rRoom tag", RoomTagCenteringCommandTag.GetMethod(), EfficiencyPack_Resources.Properties.Resources.Blue_32, EfficiencyPack_Resources.Properties.Resources.Blue_16, "Select rooms. Center tags.");
-            ButtonDataClass myButtonData9 = new ButtonDataClass("RenameView", "Rename\rView", RenameView.GetMethod(), EfficiencyPack_Resources.Properties.Resources.Blue_32, EfficiencyPack_Resources.Properties.Resources.Blue_16, "Select room. Rename View.");
-            ButtonDataClass myButtonData10 = new ButtonDataClass("DoorFireRating", "Door Fire\rRating", DoorFireRating.GetMethod(), EfficiencyPack_Resources.Properties.Resources.Blue_32, EfficiencyPack_Resources.Properties.Resources.Blue_16, "Select doors to set their fire rating.");
-            ButtonDataClass myButtonData11 = new ButtonDataClass("DoorCMU/GWB", "Door Inset\rOr Wrapped", DoorInsetWrap.GetMethod(), EfficiencyPack_Resources.Properties.Resources.Blue_32, EfficiencyPack_Resources.Properties.Resources.Blue_16, "Select doors to set whether they are inset or wrapped.");
-            ButtonDataClass myButtonData12 = new ButtonDataClass("DoorStorefrontMark", "Door Storefront\rMark", DoorStorefrontMark.GetMethod(), EfficiencyPack_Resources.Properties.Resources.Blue_32, EfficiencyPack_Resources.Properties.Resources.Blue_16, "Select storefront or curtainwall doors. Get mark based on the storefront mark.");
-            ButtonDataClass myButtonData13 = new ButtonDataClass("StorefrontElevation", "Make Storefront\rElevations", CurtainWallElevationAddIn.GetMethod(), EfficiencyPack_Resources.Properties.Resources.Blue_32, EfficiencyPack_Resources.Properties.Resources.Blue_16, "Select storefront or curtainwall to create elevations of them.");
-            ButtonDataClass myButtonData14 = new ButtonDataClass("InteriorElevation", "Make Interior\rElevations", InteriorElevation.GetMethod(), EfficiencyPack_Resources.Properties.Resources.Blue_32, EfficiencyPack_Resources.Properties.Resources.Blue_16, "Select rooms to elevate.");
-            ButtonDataClass myButtonData15 = new ButtonDataClass("FamilySizes", "Get family\rsizes in model", FamilyFileSizeReporter.GetMethod(), EfficiencyPack_Resources.Properties.Resources.Blue_32, EfficiencyPack_Resources.Properties.Resources.Blue_16, "Get sizes of all families in model.");
-            ButtonDataClass myButtonData16 = new ButtonDataClass("ModifyCropBoundaryCommand", "Raise Crop\rBoundary", ModifyCropBoundaryCommand.GetMethod(), EfficiencyPack_Resources.Properties.Resources.Blue_32, EfficiencyPack_Resources.Properties.Resources.Blue_16, "Raises the crop boundary of selected views");
-            ButtonDataClass myButtonData17 = new ButtonDataClass("FilledRegionDonut", "Create\rDonut", CreateFilledRegionCommand.GetMethod(), EfficiencyPack_Resources.Properties.Resources.Blue_32, EfficiencyPack_Resources.Properties.Resources.Blue_16, "Select views. Get Donuts.");
-            ButtonDataClass myButtonData18 = new ButtonDataClass("SetTypeImageCommand", "Set new\rType Image", SetTypeImageCommand.GetMethod(), EfficiencyPack_Resources.Properties.Resources.Blue_32, EfficiencyPack_Resources.Properties.Resources.Blue_16, "Select Family. Input Type Image.");
+            ButtonDataClass BtnFloorByDepartment = new ButtonDataClass("FloorByDepartment", "Floors by \rDepartment", FloorByDepartment.GetMethod(), EfficiencyPack_Resources.Properties.Resources.Blue_32, EfficiencyPack_Resources.Properties.Resources.Blue_16, "Select rooms. Run command. Get Floors.");
+            ButtonDataClass BtnLinesByRoom = new ButtonDataClass("LinesByRoom", "Lines by \rRoom", LinesByRoom.GetMethod(), EfficiencyPack_Resources.Properties.Resources.Blue_32, EfficiencyPack_Resources.Properties.Resources.Blue_16, "Select rooms. Run command. Get Outline.");
+            ButtonDataClass BtnRoomPlanGen = new ButtonDataClass("RoomPlanGen", "Plans by \rRoom", RoomPlanGen.GetMethod(), EfficiencyPack_Resources.Properties.Resources.Blue_32, EfficiencyPack_Resources.Properties.Resources.Blue_16, "Select rooms. Run command. Get Plans.");
+            ButtonDataClass BtnCenterRoom = new ButtonDataClass("CenterRoom", "Center\rRoom", RoomTagCenteringCommand.GetMethod(), EfficiencyPack_Resources.Properties.Resources.Blue_32, EfficiencyPack_Resources.Properties.Resources.Blue_16, "Select rooms. Center rooms.");
+            ButtonDataClass BtnCenterRoomTag = new ButtonDataClass("CenterRoomTag", "Center\rRoom tag", RoomTagCenteringCommandTag.GetMethod(), EfficiencyPack_Resources.Properties.Resources.Blue_32, EfficiencyPack_Resources.Properties.Resources.Blue_16, "Select rooms. Center tags.");
+            ButtonDataClass BtnRenameView = new ButtonDataClass("RenameView", "Rename\rView", RenameView.GetMethod(), EfficiencyPack_Resources.Properties.Resources.Blue_32, EfficiencyPack_Resources.Properties.Resources.Blue_16, "Select room. Rename View.");
+            ButtonDataClass BtnDoorFireRating = new ButtonDataClass("DoorFireRating", "Door Fire\rRating", DoorFireRating.GetMethod(), EfficiencyPack_Resources.Properties.Resources.Blue_32, EfficiencyPack_Resources.Properties.Resources.Blue_16, "Select doors to set their fire rating.");
+            ButtonDataClass BtnDoorCMUGWB = new ButtonDataClass("DoorCMU/GWB", "Door Inset\rOr Wrapped", DoorInsetWrap.GetMethod(), EfficiencyPack_Resources.Properties.Resources.Blue_32, EfficiencyPack_Resources.Properties.Resources.Blue_16, "Select doors to set whether they are inset or wrapped.");
+            ButtonDataClass BtnDoorStorefrontMark = new ButtonDataClass("DoorStorefrontMark", "Door Storefront\rMark", DoorStorefrontMark.GetMethod(), EfficiencyPack_Resources.Properties.Resources.Blue_32, EfficiencyPack_Resources.Properties.Resources.Blue_16, "Select storefront or curtainwall doors. Get mark based on the storefront mark.");
+            ButtonDataClass BtnStorefrontElevation = new ButtonDataClass("StorefrontElevation", "Make Storefront\rElevations", CurtainWallElevationAddIn.GetMethod(), EfficiencyPack_Resources.Properties.Resources.Blue_32, EfficiencyPack_Resources.Properties.Resources.Blue_16, "Select storefront or curtainwall to create elevations of them.");
+            ButtonDataClass BtnInteriorElevation = new ButtonDataClass("InteriorElevation", "Make Interior\rElevations", InteriorElevation.GetMethod(), EfficiencyPack_Resources.Properties.Resources.Blue_32, EfficiencyPack_Resources.Properties.Resources.Blue_16, "Select rooms to elevate.");
+            ButtonDataClass BtnModifyCrop = new ButtonDataClass("ModifyCropBoundaryCommand", "Raise Crop\rBoundary", ModifyCropBoundaryCommand.GetMethod(), EfficiencyPack_Resources.Properties.Resources.Blue_32, EfficiencyPack_Resources.Properties.Resources.Blue_16, "Raises the crop boundary of selected views");
+            ButtonDataClass BtnFilledRegionDonut = new ButtonDataClass("FilledRegionDonut", "Create\rDonut", CreateFilledRegionCommand.GetMethod(), EfficiencyPack_Resources.Properties.Resources.Blue_32, EfficiencyPack_Resources.Properties.Resources.Blue_16, "Select views. Get Donuts.");
+            ButtonDataClass BtnSetTypeImageCommand = new ButtonDataClass("SetTypeImageCommand", "Set new\rType Image", SetTypeImageCommand.GetMethod(), EfficiencyPack_Resources.Properties.Resources.Blue_32, EfficiencyPack_Resources.Properties.Resources.Blue_16, "Select Family. Input Type Image.");
+            ButtonDataClass BtnWorkingViews = new ButtonDataClass("WorkingViews", "Create\rworking views", WorkingViews.GetMethod(), EfficiencyPack_Resources.Properties.Resources.Blue_32, EfficiencyPack_Resources.Properties.Resources.Blue_16, "Create a set\rof new working views for you");
 
             // 4. Create buttons
-            PushButton myButton1 = panel000.AddItem(myButtonData1.Data) as PushButton;
+            PushButton myButton13 = panel000.AddItem(BtnWorkingViews.Data) as PushButton;
+            PushButton myButton1 = panel000.AddItem(BtnForestGen.Data) as PushButton;
 #if REVIT2022
-            PushButton myButton3 = panel000.AddItem(myButtonData3.Data) as PushButton;
+            PushButton myButton3 = panel000.AddItem(BtnDuplicateSheet.Data) as PushButton;
 #endif
-            PushButton myButton4 = panel000.AddItem(myButtonData4.Data) as PushButton;
-            PushButton myButton5 = panel000.AddItem(myButtonData5.Data) as PushButton;
-            PushButton myButton9 = panel000.AddItem(myButtonData9.Data) as PushButton;
-            PushButton myButton11 = panel000.AddItem(myButtonData18.Data) as PushButton;
-            PushButton mybutton12 = panel600.AddItem(myButtonData13.Data) as PushButton;
-            PushButton myButton6 = panel800.AddItem(myButtonData6.Data) as PushButton;
-            PushButton myButton2 = panel900.AddItem(myButtonData2.Data) as PushButton;
-            PushButton myButton10 = panel800.AddItem(myButtonData14.Data) as PushButton;
+            PushButton myButton4 = panel000.AddItem(BtnFloorByDepartment.Data) as PushButton;
+            PushButton myButton5 = panel000.AddItem(BtnLinesByRoom.Data) as PushButton;
+            PushButton myButton9 = panel000.AddItem(BtnRenameView.Data) as PushButton;
+            PushButton myButton11 = panel000.AddItem(BtnSetTypeImageCommand.Data) as PushButton;
+            PushButton mybutton12 = panel600.AddItem(BtnStorefrontElevation.Data) as PushButton;
+            PushButton myButton6 = panel800.AddItem(BtnRoomPlanGen.Data) as PushButton;
+            PushButton myButton2 = panel900.AddItem(BtnFloorByRoom.Data) as PushButton;
+            PushButton myButton10 = panel800.AddItem(BtnInteriorElevation.Data) as PushButton;
             //5. Split buttons
             SplitButtonData splitButtonData = new SplitButtonData("Center Room", "Center\rRoom");
             SplitButton splitButton = panel000.AddItem(splitButtonData) as SplitButton;
-            splitButton.AddPushButton(myButtonData7.Data);
-            splitButton.AddPushButton(myButtonData8.Data);
+            splitButton.AddPushButton(BtnCenterRoom.Data);
+            splitButton.AddPushButton(BtnCenterRoomTag.Data);
             SplitButtonData splitButtonData3 = new SplitButtonData("View Modifiers", "View\rModifier");
             SplitButton splitButton3 = panel800.AddItem(splitButtonData3) as SplitButton;
-            splitButton3.AddPushButton(myButtonData16.Data);
-            splitButton3.AddPushButton(myButtonData17.Data);
+            splitButton3.AddPushButton(BtnModifyCrop.Data);
+            splitButton3.AddPushButton(BtnFilledRegionDonut.Data);
             SplitButtonData splitButtonData2 = new SplitButtonData("Door Modifiers", "Door\rModifier");
             SplitButton splitButton2 = panel600.AddItem(splitButtonData2) as SplitButton;
-            splitButton2.AddPushButton(myButtonData10.Data);
-            splitButton2.AddPushButton(myButtonData11.Data);
-            splitButton2.AddPushButton(myButtonData12.Data);
+            splitButton2.AddPushButton(BtnDoorFireRating.Data);
+            splitButton2.AddPushButton(BtnDoorCMUGWB.Data);
+            splitButton2.AddPushButton(BtnDoorStorefrontMark.Data);
 
-            //PushButton myButton11 = panel000.AddItem(myButtonData15.Data) as PushButton;
 
             // NOTE:
-            // To create a new tool, copy lines 35 and 39 and rename the variables to "myButtonData3" and "myButton3". 
+            // To create a new tool, copy lines 35 and 39 and rename the variables to "BtnDuplicateSheet" and "myButton3". 
             // Change the name of the tool in the arguments of line 
 
             return Result.Succeeded;
---
+++ EfficiencyPack_Shared/WorkingViews.cs
@@ -0,0 +1,158 @@
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace EfficiencyPack
+{
+    [Transaction(TransactionMode.Manual)]
+    public class WorkingViews : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiApp = commandData.Application;
+            UIDocument uiDoc = uiApp.ActiveUIDocument;
+            //Document doc = uiDoc.Document;
+            Document doc = uiApp.ActiveUIDocument.Document;
+
+            try
+            {
+                // Step 1: Get all selected levels
+                FilteredElementCollector levelCollector = new FilteredElementCollector(doc);
+                ICollection<Element> selectedLevels = levelCollector.OfClass(typeof(Level)).ToElements();
+
+
+                // Step 2: Get the user ID of the user
+                string userId = Environment.UserName; // You can customize this as needed
+
+                // Step 3: Create a new view template for the plans and RCP
+                string planTemplateName = $"000 - {userId} - Working Floor Plan";
+                string rcpTemplateName = $"000 - {userId} - Working Ceiling Plan";
+                // Step 3: Retrieve ViewFamilyTypeIds for Plan and RCP views
+                ElementId planViewFamilyTypeId = GetViewFamilyTypeId(doc, ViewFamily.FloorPlan);
+                ElementId rcpViewFamilyTypeId = GetViewFamilyTypeId(doc, ViewFamily.CeilingPlan);
+
+                if (planViewFamilyTypeId == ElementId.InvalidElementId || rcpViewFamilyTypeId == ElementId.InvalidElementId)
+                {
+                    // Handle the case where the ViewFamilyTypeIds are not found
+                    TaskDialog.Show("Error", "Plan and/or RCP ViewFamilyType not found.");
+                    return Result.Failed;
+                }
+
+                int counter = 1;
+                using (Transaction transaction = new Transaction(doc, "Create Working Views"))
+                {
+                    transaction.Start();
+                    //Step 4 and 5: Create plan and RCP views for each level and assign templates
+                    foreach (Element levelElement in selectedLevels)
+                    {
+                        Level level = levelElement as Level;
+                        int count = 1;
+                        string planName = $"{userId} - W{counter:D2} - Working {level.Name} Plan";
+                        string RCPName = $"{userId} - W{counter:D2} - Working {level.Name} Plan";
+                        string folderName = $"000 - {userId} - Working";
+                        while (ViewNameExists(doc, planName))
+                        {
+                            planName = $"{userId}_{count} - W{counter:D2} - Working {level.Name} Plan";
+                            RCPName = $"{userId}_{count} - W{counter:D2} - Working {level.Name} Plan";
+                            folderName = $"000 - {userId}_{count} - Working";
+                            count++;
+                        }
+
+                        // Create Plan View
+                        ViewPlan planView = ViewPlan.Create(doc, planViewFamilyTypeId, level.Id);
+                        planView.Name = planName;
+
+                        // Create RCP View
+                        ViewPlan rcpView = ViewPlan.Create(doc, rcpViewFamilyTypeId, level.Id);
+                        rcpView.Name = RCPName;
+
+                        // Reset view templates to "None"
+                        ResetViewTemplate(planView);
+                        ResetViewTemplate(rcpView);
+
+                        // Set custom parameter value for the RCP view
+                        SetCustomParameter(planView, "Folder", folderName);
+                        SetCustomParameter(rcpView, "Folder", folderName);
+
+                        counter++;
+                    }
+                    transaction.Commit();
+                    TaskDialog.Show("OK!", $"You Created {(counter - 1) * 2} Plans!");
+                }
+
+                return Result.Succeeded;
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+                return Result.Failed;
+            }
+        }
+        public static String GetMethod()
+        {
+            var method = MethodBase.GetCurrentMethod().DeclaringType?.FullName;
+            return method;
+        }
+        private ElementId GetViewFamilyTypeId(Document doc, ViewFamily viewFamily)
+        {
+            // Filter the view family types by view family
+            var viewFamilyType = new FilteredElementCollector(doc)
+                .OfClass(typeof(ViewFamilyType))
+                .Cast<ViewFamilyType>()
+                .FirstOrDefault(vft => vft.ViewFamily == viewFamily);
+
+            return viewFamilyType != null ? viewFamilyType.Id : ElementId.InvalidElementId;
+        }
+        public void SetCustomParameter(View view, string parameterName, string parameterValue)
+        {
+            // Check if the view is valid
+            if (view != null)
+            {
+                // Get the parameter by name
+                Parameter parameter = view.LookupParameter(parameterName);
+
+                // Check if the parameter exists
+                if (parameter != null)
+                {
+                    // Set the parameter value as a string
+                    parameter.Set(parameterValue);
+                }
+            }
+        }
+        public void ResetViewTemplate(View view)
+        {
+            if (view != null)
+            {
+                // Unset the view template by setting the ViewTemplateId to ElementId.InvalidElementId
+                view.ViewTemplateId = ElementId.InvalidElementId;
+            }
+        }
+        private string GetUniqueViewName(Document doc, string desiredName)
+        {
+            string uniqueName = desiredName;
+            int count = 1;
+
+            while (ViewNameExists(doc, uniqueName))
+            {
+                uniqueName = $"{desiredName}_{count}";
+                count++;
+            }
+
+            return uniqueName;
+        }
+
+        private bool ViewNameExists(Document doc, string name)
+        {
+            // Check if a view with the given name already exists
+            return new FilteredElementCollector(doc)
+                .OfClass(typeof(View))
+                .WhereElementIsNotElementType()
+                .Cast<View>()
+                .Any(v => v.Name.Equals(name));
+        }
+    }
+}
